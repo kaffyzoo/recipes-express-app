ced9338a54e12d225ccf1190d347cba91012fa22
Kathy Zhou <kathy.m.zhou@gmail.com>
2025-02-04 17:42:16 -0800

    added pagination and some more test routes

--- index.js
+++ index.js
@@ -12,58 +12,70 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
   try {
-    const result = await db.query('SELECT * FROM recipe ORDER BY timestamp DESC LIMIT 3');
-    res.json(result.rows);
+    const { pageNo = 1, itemsPerPage = 2 } = req.params;
+    const result = await db.query('SELECT * FROM recipe ORDER BY timestamp DESC LIMIT $1 OFFSET $2', [itemsPerPage, (pageNo - 1) * itemsPerPage]);
+    res.status(200).json(result.rows);
   } catch (err) {
     console.error(err);
     res.status(500).send('Internal Server Error');
   }
 });
 
-app.post('/recipes', async (req, res) => {
+app.get('/recipes/:id', async (req, res) => {
   try {
-    console.log(req);
-    const { title, content } = req.body;
-
-    // Insert data into PostgreSQL
-    const result = await db.query('INSERT INTO recipe (title, content, timestamp) VALUES ($1, $2, $3) RETURNING *', [title, content, new Date()]);
-
-    res.status(201).json(result.rows[0]);
+    const { id } = req.params;
+    const result = await db.query('SELECT * FROM recipe WHERE id = $1', [id]);
+    res.status(200).json(result.rows);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).send('Internal Server Error');
   }
 });
 
-app.patch('/recipes', async (req, res) => {
-  try {
-    console.log(req);
-    const { title, id } = req.body;
 
-    // Insert data into PostgreSQL
-    const result = await db.query('UPDATE recipe SET title = $1 where id = $2', [title, id]);
+app.route('/recipes')
+  .post(async (req, res) => {
+    try {
+      console.log(req);
+      const { title, content } = req.body;
 
-    res.status(201).json(result.rows[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+      // Insert data into PostgreSQL
+      const result = await db.query('INSERT INTO recipe (title, content, timestamp) VALUES ($1, $2, $3) RETURNING *', [title, content, new Date()]);
 
-app.delete('/recipes', async (req, res) => {
-  try {
-    console.log(req);
-    const { id } = req.body;
+      res.status(201).json(result.rows[0]);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  })
+  .patch(async (req, res) => {
+    try {
+      console.log(req);
+      const { title, id } = req.body;
 
-    // Insert data into PostgreSQL
-    const result = await db.query('DELETE FROM recipe where id = $1', [id]);
+      // Insert data into PostgreSQL
+      const result = await db.query('UPDATE recipe SET title = $1 where id = $2', [title, id]);
 
-    res.status(201).json(result.rows[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+      res.status(201).json(result.rows[0]);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      console.log(req);
+      const { id } = req.body;
+
+      // Insert data into PostgreSQL
+      const result = await db.query('DELETE FROM recipe where id = $1', [id]);
+
+      res.status(201).json(result.rows[0]);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
 
 
 app.listen(port, () => {
